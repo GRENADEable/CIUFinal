0fa3979560059df4eaaa2dab0bb822c870cd7b3e
Khatim <kahtim.dar@gmail.com>
2018-12-07 20:41:02 +0400

    Imported textmesh pro asset. Menu system is done. Started working on the interaction system.

--- TheGuardian/Assets/Individual Sections/Khatim/Scripts/Managers/UIManager.cs
+++ TheGuardian/Assets/Individual Sections/Khatim/Scripts/Managers/UIManager.cs
@@ -1,11 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    public GameObject mainmenuPanel;
+    public GameObject settingsPanel;
     void Awake()
     {
-        
+        mainmenuPanel.SetActive(true);
+        settingsPanel.SetActive(false);
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene("TheGuadianGameScene");
+    }
+
+    public void Settings()
+    {
+        mainmenuPanel.SetActive(false);
+        settingsPanel.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void SettingsToMainMenu()
+    {
+        settingsPanel.SetActive(false);
+        mainmenuPanel.SetActive(true);
     }
 }
--- TheGuardian/Assets/Individual Sections/Khatim/Scripts/PlayerTestwithPhysics.cs
+++ TheGuardian/Assets/Individual Sections/Khatim/Scripts/PlayerTestwithPhysics.cs
@@ -9,7 +9,11 @@ public class PlayerTestwithPhysics : MonoBehaviour
     public float jumpForce;
     public float rotationSpeed;
     public float raycastDistance;
-    public Vector3 raycastHeight;
+    public float interactionDistance;
+    public float distance;
+    public float maxInteract;
+    public float minInteract;
+    private GameObject objectToInteract;
     private Rigidbody rg;
     void Start()
     {
@@ -18,14 +22,37 @@ public class PlayerTestwithPhysics : MonoBehaviour
 
     void Update()
     {
-
         RaycastHit hit;
         Debug.DrawRay(transform.position, -transform.up * raycastDistance, Color.green);
+        //Debug.DrawRay(transform.position, transform.forward * interactionDistance, Color.green);
+
         if (Physics.Raycast(transform.position, -transform.up, out hit, raycastDistance) && Input.GetKeyDown(KeyCode.Space))
         {
             Debug.LogWarning(hit.transform.tag);
             Jump();
         }
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit, interactionDistance) && Input.GetKeyDown(KeyCode.E) && hit.collider.tag == "Interact")
+        {
+            Debug.LogWarning(hit.transform.name);
+            hit.transform.parent = this.transform;
+            Debug.LogWarning("Object Attached");
+        }
+        else if (Physics.Raycast(transform.position, transform.forward, out hit, interactionDistance) && Input.GetKeyUp(KeyCode.E) && hit.collider.tag == "Interact")
+        {
+            hit.transform.parent = null;
+            Debug.LogWarning("Object Detached");
+        }
+
+        /*if (objectToInteract != null)
+        {
+            distance = Vector3.Distance(transform.position, objectToInteract.transform.position);
+        }
+
+        if (distance > maxInteract)
+            objectToInteract = null;
+        else if (distance < minInteract)*/
+
     }
     void FixedUpdate()
     {
@@ -43,4 +70,12 @@ public class PlayerTestwithPhysics : MonoBehaviour
     {
         rg.AddForce(transform.up * jumpForce, ForceMode.Impulse);
     }
+
+    /*void OnCollisionEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Interact" && Input.GetKeyDown(KeyCode.E))
+        {
+            objectToInteract.transform.position = other.transform.position;
+        }
+    }*/
 }
