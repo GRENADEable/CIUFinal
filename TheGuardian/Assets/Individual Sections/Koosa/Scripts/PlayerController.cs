33c24980b73eca977f4abfebb88b88b29083d323
yousef424 <yousef424ae@yahoo.com>
2018-12-30 21:06:20 +0400

    character controller fixes sprint speed and crouch speed added

--- TheGuardian/Assets/Individual Sections/Koosa/Scripts/PlayerController.cs
+++ TheGuardian/Assets/Individual Sections/Koosa/Scripts/PlayerController.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
+    public float crouchSpeed;
+    public float sprintSpeed;
+    public float walkSpeed;
     public float rotateSpeed;
     public float lockRotation = 0f;
     public float magnitudeToClamp;
@@ -12,18 +15,24 @@ public class PlayerController : MonoBehaviour
     public float climbSpeed;
 
     public bool climb = false;
+    public bool crouch = false;
+    public bool sprinting = false;
 
     public Animator playerAnimator;
 
     private float moveInput;
     private float rotateInput;
+    public float playerHeight;
 
     private Vector3 playerInput;
+    private Vector3 defaultVector;
 
     [SerializeField]
     private Rigidbody playerRb;
     [SerializeField]
-    public Collider playerCollider;
+    private Collider playerCollider;
+    [SerializeField]
+    public Transform playerTransform;
 
     private RaycastHit hit;
 
@@ -31,10 +40,16 @@ public class PlayerController : MonoBehaviour
     {
         playerRb = GetComponent<Rigidbody>();
         playerCollider = GetComponent<Collider>();
+        playerTransform = GetComponent<Transform>();
+        playerHeight = this.gameObject.transform.localScale.y;
+        defaultVector = transform.localScale;
     }
 
     void Update()
     {
+        //playerHeight = this.gameObject.transform.localScale.y;
+
+
         PlayerInputs();
         transform.rotation = Quaternion.Euler(lockRotation, transform.rotation.eulerAngles.y, lockRotation);
         playerRb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
@@ -46,10 +61,17 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         PlayerTurn();
-        if (moveInput != 0 && NotGrounded() && !climb)
+        if (moveInput != 0 && /*NotGrounded() &&*/ !climb)
         {
             PlayerMove();
         }
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Sprint();
+            Debug.Log("running running ");
+        }
+        else
+            moveSpeed = walkSpeed;
     }
 
     private void PlayerInputs()
@@ -67,8 +89,8 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 directionOfMovement = transform.forward * moveInput * moveSpeed;
         Vector3 vectorOfMovement = Vector3.ClampMagnitude(directionOfMovement, magnitudeToClamp);
-        playerRb.velocity = vectorOfMovement;
-        //playerRb.AddForce(transform.forward * moveInput * moveSpeed, ForceMode.Impulse);
+       // playerRb.velocity = vectorOfMovement;
+        playerRb.AddForce(transform.forward * moveInput * moveSpeed * Time.deltaTime, ForceMode.Impulse);
         playerRb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationY;
     }
 
@@ -76,7 +98,7 @@ public class PlayerController : MonoBehaviour
     {
         float DistanceFromGround;
         DistanceFromGround = playerCollider.bounds.extents.y;
-        return Physics.Raycast(transform.position, -Vector3.up, DistanceFromGround + 0.1f);
+        return Physics.Raycast(transform.position, -Vector3.up, DistanceFromGround);
     }
 
     private void Jump()
@@ -85,7 +107,7 @@ public class PlayerController : MonoBehaviour
         //playerRb.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ;
     }
 
-    public void OnTriggerStay(Collider collider)
+    public void OnCollisionStay(Collision collider)
     {
         if (climb)
             playerRb.velocity = Vector3.up * climbSpeed * moveInput;
@@ -100,5 +122,13 @@ public class PlayerController : MonoBehaviour
             Debug.Log(" i should kinda land now");
             climb = false;
         }
+    }
+
+
+    public void Sprint()
+    {
+        moveSpeed = sprintSpeed;
+        sprinting = true;
+        //might add more stuff
     }
 }
