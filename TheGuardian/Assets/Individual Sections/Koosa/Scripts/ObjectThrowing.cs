214f3e346ed981480c6a601d5351fc1e28dc9097
yousef424 <yousef424ae@yahoo.com>
2019-01-15 20:03:04 +0400

    changes

---
+++ TheGuardian/Assets/Individual Sections/Koosa/Scripts/ObjectThrowing.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectThrowing : MonoBehaviour
+{
+
+    public GameObject pickedUpObject;
+    public float throwingForce;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
