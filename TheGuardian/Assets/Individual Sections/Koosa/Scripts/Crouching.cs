98f2563a0f93a9df876833472c70d168d72c5674
yousef424 <yousef424ae@yahoo.com>
2019-02-21 13:26:58 +0400

    stuff

---
+++ TheGuardian/Assets/Individual Sections/Koosa/Scripts/Crouching.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crouching : MonoBehaviour
+{
+    //For Khatim
+    //uses the character controller component attatched to its gameobject
+
+    public float crouchSpeed = 5f;
+    public float normalSpeed = 6.5f; // this is the walk speed in ur playercontrols script
+    public CharacterController characterController;
+    public Vector3 playerVector;
+    public float playerHeight;
+
+	void Start ()
+    {
+        characterController = GetComponent<CharacterController>();
+        playerHeight = characterController.height;
+        playerVector = transform.position;
+    }
+	
+	void Update ()
+    {
+        float localHeight = playerHeight;
+        float localSpeed = normalSpeed;
+       // Vector3 scale = new Vector3(1, characterController.height, 1);
+       // transform.localScale = scale;
+
+        if (/*if the player is on the floor subtitute booleon here from ur script*/ Input.GetKey(KeyCode.C))
+        {
+            localHeight = playerHeight * 0.5f;
+            localSpeed = crouchSpeed;
+        }
+
+        float latestRecordedHeight = characterController.height;
+        characterController.height = Mathf.Lerp(characterController.height, localHeight, 5 * Time.deltaTime);
+        playerVector.y += (characterController.height - latestRecordedHeight) / 1.5f;
+    }
+
+    public void CrouchingCheck()
+    {
+        float localHeight = 0;
+        float localSpeed = 0;
+
+        if(/*if the player is on the floor subtitute booleon here from ur script*/ Input.GetKey(KeyCode.C))
+        {
+            CrouchingExecution(localSpeed, localHeight);
+        }
+
+    }
+
+    public void CrouchingExecution( float speed, float height)
+    {
+        height = playerHeight * 0.5f;
+        speed = crouchSpeed;
+    }
+
+    public void CharacterControllerBodyModifier()
+    {
+        float latestRecordedHeight = characterController.height;
+        //characterController.height = Mathf.Lerp(characterController.height);
+    }
+}
---
+++ TheGuardian/Assets/Individual Sections/Koosa/Scripts/LightMechanic.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightMechanic : MonoBehaviour
+{
+    public float fuel;
+    public int matches;
+    public GameObject match;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+
+        //picking up a match comes here
+        Matchlight();
+	}
+
+    void Matchlight()
+    {
+        if(Input.GetKeyDown(KeyCode.J) && matches>0 && !match.activeSelf)
+        {
+            if(fuel > 0)
+            {
+                match.SetActive(true);
+            }
+
+            if (fuel < 0)
+            {
+                match.SetActive(false);
+            }
+        }
+
+        if(match.activeSelf)
+        {
+            fuel -= Time.deltaTime;
+        }
+        if (fuel <= 0 )
+        {
+            matches = 0 ;
+            match.SetActive(false);
+        }
+    }
+}
