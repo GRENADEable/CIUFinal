48af6d2a84c171cb81bb32991ec872ae38ee7908
Khatim <kahtim.dar@gmail.com>
2019-03-08 14:24:40 +0400

    Working on fixing the player push and pull

--- TheGuardian/Assets/Individual Sections/Khatim/Scripts/Player/PlayerControlTest.cs
+++ TheGuardian/Assets/Individual Sections/Khatim/Scripts/Player/PlayerControlTest.cs
@@ -7,7 +7,9 @@ public class PlayerControlTest : MonoBehaviour
     [Header("Player Movement Variables")]
     public float walkingSpeed;
     public float runningSpeed;
+    [Header("Player Jump Variables")]
     public float jumpPower;
+    public float jumpDelay;
     [Header("Rope Variables")]
     public bool onRope;
     public float climbSpeed;
@@ -16,8 +18,16 @@ public class PlayerControlTest : MonoBehaviour
     public float defaultGravity;
     [Header("References Obejcts")]
     public GameObject trapDoor;
+    [Header("Player Push and Pull Variables")]
+    public float interactionDistance;
+    public float interactionRadius;
+    public float interactionDistanceHeight;
+
+
     private Collider col;
+    private bool isInteracting;
     private float gravity;
+    private float jumpTime;
     private CharacterController charController;
     private Vector3 moveDirection = Vector3.zero;
     [Header("Cheats Section :3")]
@@ -46,6 +56,7 @@ public class PlayerControlTest : MonoBehaviour
         }
         charController = GetComponent<CharacterController>();
         gravity = defaultGravity;
+        jumpTime = jumpDelay;
     }
     void Update()
     {
@@ -62,7 +73,35 @@ public class PlayerControlTest : MonoBehaviour
             // transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed * Time.deltaTime, 0);
             float moveHorizontal = Input.GetAxis("Vertical");
             float moveVertical = Input.GetAxis("Horizontal");
+            jumpTime += Time.deltaTime;
             // float moveCamVertical = Input.GetAxis("MoveCamVertical");
+
+            RaycastHit hit;
+            // Debug.DrawRay(transform.position + Vector3.up * interactionDistanceHeight, transform.TransformDirection(Vector3.forward) * interactionDistance, Color.blue);
+            // bool interaction = Physics.Raycast(transform.position + Vector3.up * interactionDistanceHeight, transform.TransformDirection(Vector3.forward) * interactionDistance, out hit);
+            bool interaction = Physics.SphereCast(transform.position + Vector3.up * interactionDistanceHeight, interactionRadius, transform.TransformDirection(Vector3.forward), out hit, interactionDistance);
+
+            if (interaction && Input.GetKey(KeyCode.E) && hit.collider.tag == "Interact" && !isInteracting)
+            {
+                // Sets bool to true, adds fixed joint component and links fixed joint from other gameobject to ours and turns off gravity.
+                hit.collider.gameObject.AddComponent(typeof(FixedJoint));
+                hit.collider.gameObject.GetComponent<FixedJoint>().enableCollision = true;
+                hit.collider.gameObject.GetComponent<FixedJoint>().connectedBody = this.gameObject.GetComponent<Rigidbody>();
+                hit.rigidbody.isKinematic = false;
+                hit.rigidbody.useGravity = false;
+                isInteracting = true;
+                Debug.LogWarning("Object Attached");
+            }
+
+            if (Input.GetKeyUp(KeyCode.E) && isInteracting && hit.collider.tag == "Interact")
+            {
+                // Sets bool to false, removes fixed joint from the other gameobject with ours, turns on  gravity of the other object and destroys the fixed joint component.
+                Destroy(hit.collider.gameObject.GetComponent<FixedJoint>());
+                hit.rigidbody.useGravity = true;
+                hit.rigidbody.isKinematic = true;
+                isInteracting = false;
+                Debug.LogWarning("Object Detached");
+            }
 
             if (charController.isGrounded)
             {
@@ -74,7 +113,7 @@ public class PlayerControlTest : MonoBehaviour
                 // moveDirection = transform.TransformDirection(moveDirection);
                 moveDirection = new Vector3(-moveHorizontal, 0.0f, moveVertical);
 
-                if (moveDirection != Vector3.zero)
+                if (moveDirection != Vector3.zero && !isInteracting)
                     transform.rotation = Quaternion.Lerp(this.transform.rotation, Quaternion.LookRotation(moveDirection), 0.15f);
 
                 if (Input.GetKey(KeyCode.LeftShift))
@@ -88,9 +127,9 @@ public class PlayerControlTest : MonoBehaviour
                     // Debug.LogWarning("Walking");
                 }
 
-                if (Input.GetKey(KeyCode.Space))
+                if (Input.GetKey(KeyCode.Space) && !isInteracting)
                 {
-                    moveDirection.y = jumpPower;
+                    Jump();
                     // Debug.LogWarning("Jump");
                 }
             }
@@ -124,6 +163,16 @@ public class PlayerControlTest : MonoBehaviour
 
         if (onRope && Input.GetKeyUp(KeyCode.E) && !charController.isGrounded)
             onRope = false;
+    }
+
+    void Jump()
+    {
+        if (jumpTime > jumpDelay)
+        {
+            moveDirection.y = jumpPower;
+            // Debug.LogWarning("Jump");
+            jumpTime = 0f;
+        }
     }
 
     // Function which checks what hit the Character Controller's Collider
