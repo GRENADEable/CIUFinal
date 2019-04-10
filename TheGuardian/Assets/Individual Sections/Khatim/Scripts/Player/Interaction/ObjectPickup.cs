7c3fd0d3e718e6359f67cefd90fa7d15ea50c549
yousef424 <yousef424ae@yahoo.com>
2019-04-10 17:12:14 +0400

    Interaction/ObjectPickup.cs

--- TheGuardian/Assets/Individual Sections/Khatim/Scripts/Player/Interaction/ObjectPickup.cs
+++ TheGuardian/Assets/Individual Sections/Khatim/Scripts/Player/Interaction/ObjectPickup.cs
@@ -4,54 +4,36 @@ using UnityEngine;
 
 public class ObjectPickup : PlayerInteraction
 {
+    public Transform pivotDummy;
+    public Rigidbody rgCourageRightHand;
+    public FixedJoint objectFixedJoint;
+    public float throwingForce;
+
+
     public override void StartInteraction()
     {
         base.StartInteraction();
-        Debug.LogWarning("Object Pickup Started");
+        interactCol.gameObject.AddComponent(typeof(FixedJoint));
+        objectFixedJoint = interactCol.GetComponent<FixedJoint>();
+        interactCol.transform.position = pivotDummy.position;
+        objectFixedJoint.connectedBody = rgCourageRightHand;
+        rgObject.useGravity = false;
+        Debug.LogWarning("Object Picked Up");
     }
-
     public override void UpdateInteraction()
     {
-        
-        Debug.LogWarning("Object Pickup In Progress");
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Destroy(objectFixedJoint);
+            rgObject.AddForce(this.gameObject.transform.up * throwingForce + this.gameObject.transform.forward * throwingForce, ForceMode.Impulse);
+            rgObject.useGravity = true;
+            Debug.LogWarning("Object Thrown");
+        }
     }
-
     public override void EndInteraction()
     {
+        rgObject.useGravity = true;
+        Destroy(objectFixedJoint);
         Debug.LogWarning("Object Pickup Ended");
     }
-
-   // void ObjectPickup()
-    // {
-    //     //Replaced it with trigger collider because the raycast was not accurate when the distance was increased or decreased.
-    //     interactCol.gameObject.AddComponent(typeof(FixedJoint));
-    //     interactCol.transform.position = pivotDummy.position;
-
-    //     interactCol.gameObject.GetComponent<FixedJoint>().connectedBody = rgCourageRightHand;
-    //     interactCol.GetComponent<Rigidbody>().useGravity = false;
-    //     Debug.LogWarning("Object Picked Up");
-    // }
-
-    // void ObjectDrop()
-    // {
-    //     //To avoid the three lines of code to not run. I moved those three lines of code under the DropObject Class.
-    //     // if (onObjectDropEvent != null)
-    //     // {
-    //     //     isPickingObject = false;
-    //     //     onObjectDropEvent();
-    //     // }
-
-    //     interactCol.GetComponent<Rigidbody>().useGravity = true;
-    //     Destroy(interactCol.gameObject.GetComponent<FixedJoint>());
-    //     Debug.LogWarning("Object LetGo");
-    // }
-
-    // void ObjectThrow()
-    // {
-    //     Destroy(interactCol.gameObject.GetComponent<FixedJoint>());
-    //     Rigidbody objectRg = interactCol.GetComponent<Rigidbody>();
-    //     objectRg.AddForce(this.gameObject.transform.up * throwingForce + this.gameObject.transform.forward * throwingForce, ForceMode.Impulse);
-    //     objectRg.useGravity = true;
-    //     Debug.LogWarning("Object Thrown");
-    // }
 }
