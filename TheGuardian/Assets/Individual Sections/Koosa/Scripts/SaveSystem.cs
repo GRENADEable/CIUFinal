96f4fdb7e878e152e014da4ddf8697daa7bac6db
yousef424 <yousef424ae@yahoo.com>
2019-05-01 13:32:23 +0400

    ..

--- TheGuardian/Assets/Individual Sections/Koosa/Scripts/SaveSystem.cs
+++ TheGuardian/Assets/Individual Sections/Koosa/Scripts/SaveSystem.cs
@@ -5,33 +5,41 @@ using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 
 public  class SaveSystem : MonoBehaviour
-{
-    static void SaveCharacter(GameObject data)
+{ 
+    public void Start()
     {
-        PlayerPrefs.SetFloat("vector1player" ,data.transform.position.x);
-        PlayerPrefs.SetFloat("vector2player" , data.transform.position.y);
-        PlayerPrefs.SetFloat("vector3player" , data.transform.position.z);
+        DontDestroyOnLoad(this.gameObject);
+    }
+    public void SaveCharacter(GameObject data, int slot)
+    {
+        PlayerPrefs.SetFloat("vector1player"+ slot ,data.transform.position.x);
+        PlayerPrefs.SetFloat("vector2player" + slot, data.transform.position.y);
+        PlayerPrefs.SetFloat("vector3player" + slot, data.transform.position.z);
     }
 
-    static void SaveKeyPosition(GameObject data)
+    public void SaveKeyPosition(GameObject data, int slot)
     {
-        PlayerPrefs.SetFloat("vector1key", data.transform.position.x);
-        PlayerPrefs.SetFloat("vector2key", data.transform.position.y);
-        PlayerPrefs.SetFloat("vector3key", data.transform.position.z);
+        PlayerPrefs.SetFloat("vector1key" + slot, data.transform.position.x);
+        PlayerPrefs.SetFloat("vector2key" + slot, data.transform.position.y);
+        PlayerPrefs.SetFloat("vector3key" + slot, data.transform.position.z);
     }
 
-    static void Loadharacter(GameObject data)
+    public Vector3 Loadharacter(int slot)
     {
-        PlayerPrefs.SetFloat("vector1player", data.transform.position.x);
-        PlayerPrefs.SetFloat("vector2player", data.transform.position.y);
-        PlayerPrefs.SetFloat("vector3player", data.transform.position.z);
+        float x  =PlayerPrefs.GetFloat("vector1player" + slot);
+        float y = PlayerPrefs.GetFloat("vector2player" + slot);
+        float z = PlayerPrefs.GetFloat("vector3player" + slot);
+        Vector3 position = new Vector3(x, y, z);
+        return position;
     }
 
-    static void LoadKeyPosition(GameObject data)
+    public Vector3 LoadKeyPosition( int slot)
     {
-        PlayerPrefs.SetFloat("vector1key", data.transform.position.x);
-        PlayerPrefs.SetFloat("vector2key", data.transform.position.y);
-        PlayerPrefs.SetFloat("vector3key", data.transform.position.z);
+        float x = PlayerPrefs.GetFloat("vector1key" + slot);
+        float y = PlayerPrefs.GetFloat("vector2key" + slot);
+        float z = PlayerPrefs.GetFloat("vector3key" + slot);
+        Vector3 position = new Vector3(x, y, z);
+        return position;
     }
 }
 	
